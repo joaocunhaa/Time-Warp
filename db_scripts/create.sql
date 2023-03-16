d9d90238d21250c87fe09437f5c2664008fb62a9
Khevynn Sá <115984821+WinterTalon@users.noreply.github.com>
2023-03-16 17:26:27 +0000

    Database Updated

--- db_scripts/create.sql
+++ db_scripts/create.sql
@@ -1,6 +1,7 @@
-create database mygame;
+drop database if exists time_warp;
+create database time_warp;
 
-use mygame;
+use prototypeTest;
 
 create table user (
     usr_id int not null auto_increment,
@@ -13,7 +14,44 @@ create table game (
     gm_id int not null auto_increment,
     gm_turn int not null default 1,
     gm_state_id int not null,
+    gm_reversed_board boolean default false,
     primary key (gm_id));
+
+create table card(
+    crd_id int not null AUTO_INCREMENT,
+    crd_name VARCHAR(60) not null,
+    crd_description VARCHAR(200),
+    primary key(crd_id)
+);
+
+create table artifact(
+    art_id int not null AUTO_INCREMENT,
+    art_name VARCHAR(60) not NULL,
+    art_era_id int not null,
+    primary key(art_id)
+);
+
+create table era(
+    era_id int not null AUTO_INCREMENT,
+    era_name VARCHAR(60) not null,
+    primary key(era_id)
+);
+
+create table user_game_card(
+    ugc_id int not null AUTO_INCREMENT,
+    ugc_ug_id int not null,
+    ugc_crd_id int not null,
+    primary key(ugc_id)
+);
+
+create table game_artifact(
+    ga_id int not null AUTO_INCREMENT,
+    ga_gm_id int not null,
+    ga_art_id int not null,
+    ga_current_owner int,
+    ga_current_position int,
+    primary key(ga_id)
+);
 
 create table game_state (
     gst_id int not null auto_increment,
@@ -25,6 +63,7 @@ create table user_game (
     ug_user_id int not null,
     ug_game_id int not null,
     ug_state_id int not null,
+    ug_current_position int not null default 1,
     primary key (ug_id));
 
 create table user_game_state (
@@ -49,3 +88,27 @@ alter table user_game add constraint user_game_fk_game
 alter table user_game add constraint user_game_fk_user_game_state
             foreign key (ug_state_id) references user_game_state(ugst_id) 
 			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table artifact add constraint artifact_fk_era
+            foreign key (art_era_id) references era(era_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table user_game_card add constraint ugc_fk_ug
+            foreign key (ugc_ug_id) references user_game(ug_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table user_game_card add constraint ugc_fk_card
+            foreign key (ugc_crd_id) references card(crd_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table game_artifact add constraint ga_fk_gm
+            foreign key (ga_gm_id) references game(gm_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table game_artifact add constraint ga_fk_artifact
+            foreign key (ga_art_id) references artifact(art_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table game_artifact add constraint ga_fk_ug
+            foreign key (ga_current_owner) references user_game(ug_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
--- db_scripts/populate.sql
+++ db_scripts/populate.sql
@@ -1,15 +1,35 @@
-# Do not change the order or names of states 
+#Do not change the order or names of states 
 #(the code is assuming specific IDs and names)
-# You can add more in the end
+#You can add more in the end
 insert into game_state (gst_state) values ('Waiting');
 insert into game_state (gst_state) values ('Started');
 insert into game_state (gst_state) values ('Finished');
 insert into game_state (gst_state) values ('Canceled');
 
-# Do not change the order, but you can add more in the end
+#Do not change the order, but you can add more in the end
 insert into user_game_state (ugst_state) values ('Waiting');
 insert into user_game_state (ugst_state) values ('Playing');
 insert into user_game_state (ugst_state) values ('End');
 
+# Cards
+insert into card(crd_name, crd_description) values ('Claim Artifact', 'This card claims the artifact of the current era');
+insert into card(crd_name, crd_description) values ('Steal Artifact', 'This card steals a random artifact of the opponent');
 
+#Eras
+insert into era(era_name) values ('Egyptian Era');
+insert into era(era_name) values ('Ancient Greece');
+insert into era(era_name) values ('Roman Empire');
+insert into era(era_name) values ('Feudal Japan');
+insert into era(era_name) values ('Shogunate Era');
+insert into era(era_name) values ('Second Industrial Revolution');
+insert into era(era_name) values ('Information Age');
 
+#Artifacts
+insert into artifact(art_name, art_era_id) values ('Egyptian Scarab',1);
+insert into artifact(art_name, art_era_id) values ('Greece Armor',2);
+insert into artifact(art_name, art_era_id) values ('Roman Armor',3);
+insert into artifact(art_name, art_era_id) values ('Samurai Sword',4);
+insert into artifact(art_name, art_era_id) values ('Shogun Weapon',5);
+insert into artifact(art_name, art_era_id) values ('Industrial Wheels',6);
+insert into artifact(art_name, art_era_id) values ('Flamethrower',7);
+--insert into user_game_card(ugc_ug_id, ugc_crd_id) values (1,2);
