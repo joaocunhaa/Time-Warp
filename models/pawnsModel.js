76b1179b0d2e1f1a98ed4d0e53db1aa9ebd3c662
Khevynn Sá <115984821+WinterTalon@users.noreply.github.com>
2023-03-17 14:48:13 +0000

    Move Pawn Function
    Working Properly (JUST SERVER SIDE)

--- models/pawnsModel.js
+++ models/pawnsModel.js
@@ -21,14 +21,27 @@ class Pawn{
         }
     }
 
-    // static async movePawn(game){
-    //     try{
-
-    //     } catch(err) {
-    //         console.log(err);
-    //         return{status: 500, result: err}
-    //     }
-    // }
+    static async movePawn(game){
+        try{
+            if(game.player.state.name != "Playing"){
+                return{status: 400, result: {msg: "You cant play, since is not your turn"}}
+            }
+            let [gameInfo] = await pool.query('select * from game where gm_id = ?', [game.id]);
+            let [playerInfo] = await pool.query('select * from user_game where ug_id = ?', [game.player.id]);
+            let nextPosition = 0;
+            console.log(playerInfo);
+            if (gameInfo.gm_reversed_board == true){
+                nextPosition = playerInfo[0].ug_current_position - 1;
+            }else{
+                nextPosition = playerInfo[0].ug_current_position + 1;
+            }
+            await pool.query('update user_game set ug_current_position = ? where ug_id = ?', [nextPosition, game.player.id]);
+            return{status:200, result: {msg: "Succesfully moved"}}
+        } catch(err) {
+            console.log(err);
+            return{status: 500, result: err}
+        }
+    }
 }
 
 
--- routes/pawnsRoutes.js
+++ routes/pawnsRoutes.js
@@ -17,5 +17,19 @@ router.get("/", auth.verifyAuth, async function(req,res,next){
     }
 });
 
+router.patch("/", auth.verifyAuth, async function(req,res,next){
+    try{
+        if(!req.game){
+            res.status(401).send({msg: "You are not in a game"})
+        }else{
+            let result = await Pawn.movePawn(req.game);
+            res.status(result.status).send(result.result);
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+});
+
 
 module.exports = router;
