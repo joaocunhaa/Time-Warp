9bbd07016cb150f3ce62599911eb5ab939350cf0
Khevynn Sá <115984821+WinterTalon@users.noreply.github.com>
2023-03-21 15:16:15 +0000

    Server Side Update
    -Play card half-done

--- models/cardsModel.js
+++ models/cardsModel.js
@@ -51,6 +51,41 @@ class Card{
             return{status:500, result: err}
         }
     }
+
+    static async playCard(game, selectedCard, selectedArtifact){
+        try{
+            //Verify if its player's turn
+            if(game.player.state == "Waiting"){
+                return{status: 400, result: {msg: "You can't play since its not your turn!"}}
+            }
+            let [cards] = await pool.query('select * from user_game_card where ugc_ug_id = ?', [game.player.id]);
+            for(let card of cards){
+                if(card.ugc_id == selectedCard){
+                    switch(card.ugc_crd_id){
+                        case 1:
+                            let [playerInfo] = await pool.query('select * from user_game where ug_id = ?', [game.player.id]);
+                            claimArtifact(playerInfo[0].ug_current_position);
+                            break;
+                        case 2:
+                            stealArtifact(selectedArtifact);
+                            break;
+                    }
+                }
+            }
+            return{status:200, result: {msg: "Card played succesfully"}}
+        } catch(err) {
+            console.log(err);
+            return{status: 200, result:err}
+        }
+    }
+}
+
+async function claimArtifact(position){
+
+}
+
+async function stealArtifact(selectedArtifact){
+
 }
 
 module.exports = Card;
