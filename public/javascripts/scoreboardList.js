3c7f829de6a9c53e672419378143ae430ea36825
John <32777464+joaocunhaa@users.noreply.github.com>
2023-03-28 11:01:06 +0100

    UI
    - Match history menu finished
    - Matches menu changed
    - Start of refresh button

--- public/javascripts/scoreboardList.js
+++ public/javascripts/scoreboardList.js
@@ -1,5 +1,8 @@
 window.onload = async function() {
-    let result = await requestScores();
+    let result = await checkAuthenticated(true);
+    if (result.err) { throw result.err; }
+    document.getElementById('player').textContent = "Hello " + window.user.name;
+    result = await requestScores();
     if (!result.successful || result.err) {
         alert("Something wrong. Going to login page");
         window.location.pathname = "/index.html"
@@ -11,29 +14,29 @@ function fillScores(scores) {
     let container = document.getElementById("scores");
     for (let score of scores) {
         let elem = document.createElement("section");
-        let t = document.createElement("table");
-        elem.appendChild(t);
-        let tr = document.createElement("tr");
-        t.appendChild(tr); 
-        let td = document.createElement("td");
-        t.appendChild(td); 
-        td.colSpan = 3;
-        td.style.textAlign = "center";
-        td.style.borderBottom = "2px solid black";
-        td.textContent = "Game "+score.gameId;
         for (let player of score.playerScores) {
-            let tr = document.createElement("tr");
-            t.appendChild(tr);
-            let td = document.createElement("td");
-            td.textContent = player.name;
-            tr.appendChild(td);
-            td = document.createElement("td");
-            td.textContent = player.state;
-            tr.appendChild(td);
-            td = document.createElement("td");
-            td.textContent = "Points: "+player.points;
-            tr.appendChild(td);
+            let p = document.createElement("p");
+            p.textContent = "GAME " + score.gameId + " - You "+ " " + player.state;
+            elem.appendChild(p);
+
+            if (player.state == "Won") {
+                elem.setAttribute("class", "won");
+            } else if (player.state == "Lost") {
+                elem.setAttribute("class", "lost");
+            }
         }
         container.appendChild(elem);
+    }
+}
+
+async function logout() {
+    try {
+        let result = await requestLogout();
+        if (!result.successful || result.err)
+            throw result.err || { err: "Not successfull" }
+        window.location.pathname = "/index.html"
+    } catch (err) {
+        console.log(err);
+        // alert("Something is not working");
     }
 }
