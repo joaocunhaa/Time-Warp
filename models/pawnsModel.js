9622c9d86be299f3e136b05cfd3fa52802623329
Khevynn Sá <115984821+Khevynn@users.noreply.github.com>
2023-04-28 12:55:43 +0100

    Server Side Update
    -New feature: players swap their artifacts when they meet each other

--- models/pawnsModel.js
+++ models/pawnsModel.js
@@ -44,6 +44,11 @@ class Pawn {
 
             await pool.query('update user_game set ug_current_position = ? where ug_id = ?', [nextPosition, game.player.id]);
 
+            if(nextPosition == game.opponents[0].position){
+                let result = await swapArtifacts(game);
+                return{status: 200, result:{msg: "Succesfully moved", swap: result.msg}}
+            }
+
             return { status: 200, result: { msg: "Succesfully moved" } }
         } catch (err) {
             console.log(err);
@@ -62,6 +67,26 @@ class Pawn {
             return { status: 500, result: err };
         }
     }
+}
+
+async function swapArtifacts(game){
+    let [playerArtifacts] = await pool.query('select * from game_artifact where ga_gm_id = ? and ga_current_owner = ?', [game.id, game.player.id]);
+    let [oppArtifacts] = await pool.query('select * from game_artifact where ga_gm_id = ? and ga_current_owner = ?', [game.id, game.opponents[0].id]);
+
+    //Verify if both has artifacts
+    if(!playerArtifacts.length && !oppArtifacts.length){
+        return{succesfull: false, msg:"Both player has no artifacts"}
+    }
+    //Pass player's artifacts to enemy
+    for(let artifact of playerArtifacts){
+        await pool.query('update game_artifact set ga_current_owner = ? where ga_id = ?', [game.opponents[0].id, artifact.ga_id]);
+    }
+    //Pass enemy's artifacts to you
+    for(let artifact of oppArtifacts){
+        await pool.query('update game_artifact set ga_current_owner = ? where ga_id = ?', [game.player.id, artifact.ga_id]);
+    }
+
+    return{succesfull: true, msg:"Artifacts switched succcesfully"}
 }
 
 module.exports = Pawn;
